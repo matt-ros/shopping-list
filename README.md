c66651a96a18ec235f9f20c2820d683aa78d75c3
Matt Rosensweig <matthew.rosensweig@gmail.com>
2020-08-03 21:47:24 -0400

    App is now functional per assignment requirements

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Shopping List App Challenge
 
 This repo contains starter files for the *Shopping List App* challenge.
+
+## My Changes
+
+I have made all changes requested in the assignment.
---
+++ index.js
@@ -0,0 +1,16 @@
+$(function () {
+
+    $('#js-shopping-list-form').submit(function(event) {
+        event.preventDefault();
+        $('ul.shopping-list').append(`<li><span class="shopping-item">${$('#shopping-list-entry').val()}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
+        $('#shopping-list-entry').val('');
+    });
+
+    $('ul').on('click', '.shopping-item-toggle', function(event) {
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    });
+
+    $('ul').on('click', '.shopping-item-delete', function(event) {
+        $(this).closest('li').remove();
+    });
+});
